3efadf1479b8ebfd745cd3fffcc3d78d1e6498a1
shadcarlson07 <shad.carlson@aggiemail.usu.edu>
2017-03-29 16:42:33 -0600

    final

---
+++ SportsStore/SportsStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IproductsRepository repository;
+        private IOrderProcessor orderProcessor;
+
+        public CartController(IproductsRepository repo, IOrderProcessor proc)
+        {
+            repository = repo;
+            orderProcessor = proc;
+        }
+
+        public ViewResult Index(Cart cart,string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                ReturnUrl = returnUrl,
+                Cart = cart
+            });
+        }
+
+        public RedirectToRouteResult AddToCart(Cart cart, int productId, string returnUrl, int quanityAdd)
+        {
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null)
+            {
+                cart.AddItem(product, quanityAdd);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(Cart cart, int productId, string returnUrl)
+        {
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null)
+            {
+                cart.RemoveLine(product);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+
+        public PartialViewResult Summary(Cart cart)
+        {
+            return PartialView(cart);
+        }
+
+        public ViewResult Checkout()
+        {
+            return View(new ShippingDetails());
+        }
+
+        [HttpPost]
+        public ViewResult Checkout(Cart cart, ShippingDetails shippingDetails)
+        {
+            if (cart.Lines.Count() ==0)
+            {
+                ModelState.AddModelError("", "Sorry, your cart is empty!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                orderProcessor.ProcessOrder(cart, shippingDetails);
+                cart.Clear();
+                return View("Completed");
+            } else
+            {
+                return View(shippingDetails);
+            }
+        }
+    }
+}
---
+++ SportsStore/SportsStore.WebUI/HtmlHelpers/PagingHelpers.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Text;
+using SportsStore.WebUI.Models;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.HtmlHelpers
+{
+    public static class PagingHelpers
+    {
+        public static MvcHtmlString PageLinks(this HtmlHelper html,
+                                                PagingInfo pagingInfo,
+                                                Func<int, string> pageUrl)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i =1; i <= pagingInfo.TotalPages; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.MergeAttribute("href", pageUrl(i));
+                tag.InnerHtml = i.ToString();
+                if (i == pagingInfo.CurrentPage)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
+                }
+                tag.AddCssClass("btn btn-default");
+                result.Append(tag.ToString());
+            }
+            return MvcHtmlString.Create(result.ToString());
+        }
+
+        public static MvcHtmlString PagedProductGrid(this HtmlHelper html, PagingInfo pagingInfo, IEnumerable<Product> products)
+        {
+            StringBuilder result = new StringBuilder();
+            var gridProducts=
+            products.OrderBy(p => p.ProductID)
+                .Skip((pagingInfo.CurrentPage - 1) * pagingInfo.ItemsPerPage)
+                .Take(pagingInfo.ItemsPerPage);
+
+            foreach (var product in gridProducts)
+            {
+                TagBuilder h2 = new TagBuilder("h2");
+                TagBuilder p = new TagBuilder("p");
+
+                h2.InnerHtml = product.Name + " (" + product.Price.ToString("C") + ")";
+                p.InnerHtml = product.Description;
+
+                TagBuilder div = new TagBuilder("div");
+                div.InnerHtml = h2.ToString() + p.ToString();
+                div.AddCssClass("well");
+
+                TagBuilder br = new TagBuilder("br");
+
+                result.Append(div.ToString() + br.ToString());
+            }
+
+            return MvcHtmlString.Create(result.ToString());
+        }
+    }
+}
